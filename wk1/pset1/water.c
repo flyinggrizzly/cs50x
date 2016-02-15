bee85ed3aca53dacbbbc7319409f72b23e723985
Sean.DMR <sean@flyinggrizzly.io>
2016-02-15 19:19:47 +0000

    finished water.c problem

---
+++ wk1/pset1/water.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    
+    // init variable as number of bottles of water per minute of shower
+    // 1 minute = 12 bottles
+    int bpm = 12;
+    
+    // prompt user for shower lenght, in minutes
+    printf("How long do you shower, in minutes? (round up to nearest minute please)\n");
+    int mins = GetInt();
+    
+    // output product of mins and bpm
+    printf("You use the equivalent of %i bottles of water when you shower for %i minutes.\n", bpm*mins, mins);
+    
+}
