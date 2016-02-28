c55fd306b6f488e935150857bbb41b372cefe6ad
Sean David Moran-Richards <sean@flyinggrizzly.io>
2016-02-28 19:03:48 +0000

    add caesar

---
+++ wk2/pset2/caesar.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Caesar Cipher
+ * 
+ * (C) 2016 Sean DMR//Flying Grizzly
+ * 
+ * Submitted to CS50X as part of PSet 2
+ *
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <cs50.h>
+
+// prototypes
+int ValidateInput(int argc, string argv[]);
+
+// main
+int main(int argc, string argv[]) {
+    
+    // pass inputs through to validator function
+    
+    
+
+}
+
+int ValidateInput(int argc, string argv[]) {
+    
+    // ensure we receive some value k with which to encrypt
+    if (argc != 2) {
+        return 1;
+    }
+    else {
+        
+        int k = atoi(argv[2])
+        if ()
+    }
+}
