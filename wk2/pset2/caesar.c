42fd5c9b7a8669c93a591da779e5729ca0668b4b
Sean David Moran-Richards <sean@flyinggrizzly.io>
2016-02-28 19:37:17 +0000

    skeleton of key validation complete

--- wk2/pset2/caesar.c
+++ wk2/pset2/caesar.c
@@ -25,15 +25,47 @@ int main(int argc, string argv[]) {
 
 }
 
+// validates and returns a key. If error, returns -1.
 int ValidateInput(int argc, string argv[]) {
     
     // ensure we receive some value k with which to encrypt
     if (argc != 2) {
-        return 1;
+        return -1;
     }
     else {
-        
-        int k = atoi(argv[2])
-        if ()
+        // ensure string at argv[1] is length 1, contains digit
+        if (strlen(argv[1]) != 1) {
+            return -1;
+        }
+        else {
+            
+            string s = argv[1];
+          
+            if (isdigit(s[0])) {
+            
+                int k = atoi(s[0]);
+                if (k > 0) {
+                    return k;
+                }
+                else {
+                    return -1;
+                }
+            }
+            else {
+                return -1;
+            }
+        }
     }
 }
+
+/* TODO
+
+1. validate input
+    1.1 argc == 2
+    1.2 argv[1] == counting number
+2. convert argv[1] to int
+    NB: this actually needs to happen to validate that it is a counting number
+3. 
+
+
+*/
